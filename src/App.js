adddb61b1916cf3ff297585333bae677b52d6417
SFoskitt <stephanie.foskitt@gmail.com>
2017-03-11 19:59:07 -0800

    changed this enough to get some quiz questions from public API

--- src/App.js
+++ src/App.js
@@ -2,27 +2,44 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import List from './List';
-import { tasks } from './tasks_data';
+// temp hold tasks data while quiz questions
+// import { tasks } from './tasks_data';
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      tasks: tasks
+      questions: []
     }
+    this.handleGetQuestions = this.handleGetQuestions.bind(this);
+  }
+
+  handleGetQuestions() {
+    fetch('/api.php?amount=5&category=17&difficulty=easy&type=multiple')
+    .then(response => { 
+      if(response.ok){
+        return response.json();
+      }
+    })
+    .then(data => {
+      this.setState({
+        questions: data.results
+      })
+    })
   }
 
   render() {
-    console.log('this.state', this.state);
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
           <h2>Crazy-Simple Task List</h2>
         </div>
-        <p className="App-intro">
-          <List tasks={this.state.tasks}/>
-        </p>
+        <div className="App-intro">
+          <List getQuestions={this.handleGetQuestions} questions={this.state.questions} />
+          {/* temp hold data forward to List while quiz questions */}
+          {/* <List tasks={this.state.tasks}/> */}
+        </div>
       </div>
     );
   }
--- src/List.js
+++ src/List.js
@@ -2,15 +2,60 @@ import React, { Component } from 'react';
 import Task from './Task';
 
 class List extends Component {
-  render () {
+
+  componentDidMount() {
+    this.props.getQuestions();
+  }
+
+  render() {
     return (
       <div>
-        {this.props.tasks.map(task => {
+        {/* temp hold tasks list while quiz questions */}
+        {/*{this.props.tasks.map(task => {
           return <Task key={task.id} title={task.title} />
+        })}*/}
+        {this.props.questions.map((question) => {
+          return <Task title={question.question} />
         })}
       </div>
     )
   }
+
+  // constructor () {
+  //   super();
+    // var ans;
+    // fetch('/api.php?amount=5&category=17&difficulty=easy&type=multiple')
+    //   .then(response => { 
+    //     if(response.ok){
+    //       return response.json();
+    //     }
+    //   })
+    //   .then(data => {
+    //     console.log('data.results', data.results);
+    //     ans = data.results;
+    //   });
+    //   console.log('ans?', ans);
+    //   this.state = {
+    //     questions: ans
+    //   }
+  // }
+
+  // componentWillMount(){
+    // fetch('/api.php?amount=5&category=17&difficulty=easy&type=multiple')
+    // .then(response => { 
+    //   if(response.ok){
+    //     return response.json();
+    //   }
+    // })
+    // .then(data => {
+    //   console.log('data.results', data.results);
+    //   this.ans = data.results;
+    //   // this.state = {
+    //   //   questions: data.results
+    //   // }
+    // })
+  // }
+
 }
 
 export default List;
